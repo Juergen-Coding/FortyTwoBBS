f960b5cb82a3b159ca1f0d52c4b23710ebfcb02b
Michael Dillon <gsvalore@users.sourceforge.net>
2021-12-16 04:54:45 -0600

    changed: reverted a fix for a FreeBSD warning for further investigation

--- lib/mbselib.h
+++ lib/mbselib.h
@@ -187,7 +187,8 @@ struct icmp_filter {
 /*
  * Pragma pack is to make all databases portable
  */
-#pragma pack(push, 1)
+/*#pragma pack(push, 1)*/
+#pragma pack(1)
 
 
 /*****************************************************************************
@@ -2729,5 +2730,5 @@ extern struct	_ibcsrvhdr	ibcsrvhdr;		/* IBC servers		    */
 extern struct	_ibcsrv		ibcsrv;
 
 
-#pragma pack(pop)
+/* #pragma pack(pop) */
 #endif
